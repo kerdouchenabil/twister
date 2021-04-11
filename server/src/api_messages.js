92080c024930cdb6a6377b0ab39c2237906bd60c
kerdouchenabil <nabil.kerdouche@etu.upmc.fr>
2021-04-12 00:21:37 +0200

    list all messages OK /TODO:only_friends

--- server/src/api_messages.js
+++ server/src/api_messages.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const Messages = require("./entities/messages.js");
 
+const bodyParser = require('body-parser');
+const url = require('url');
+const querystring = require('querystring');
+
+
 function init(db) {
     const router = express.Router();
     // On utilise JSON
@@ -53,7 +58,58 @@ function init(db) {
                     details: (error || "Erreur inconnue").toString()
                 })
             }
-        })
+        });
+
+        
+    //----------------------- list messages --------------------------
+    router.route("/messages/:max(\\d+):onlyfriends")
+    .get( async (req, res) => { // pas de body, utiliser url query
+        try{
+            /*
+            //recuperer les params dans l'url ?... avec querystring
+            console.log("req.query=",req.query) //test
+            let query = JSON.stringify(req.query)
+            const { max_time, friends_only } = query;
+*/
+            let max_time=req.params.max
+            let friends_only=req.params.onlyfriends
+            console.log("max_time=",max_time, "  friends_only", friends_only) //test
+
+            // si pas de text ET pas de file
+            if (!max_time && !friends_only) {
+                res.status(404).json({
+                    //status: 401, //facultatif
+                    message: "Requête invalide : choose max_time and friends_only values !"
+                });
+                return;
+            }
+
+            // voir si on va permettre aux non connectés de voir les messages, ici non
+            // utilisateur connecté ?
+            if (! req.session.userid ) {
+                res.status(401).json({message:"post message: utilisateur non authentifié"});
+                return;
+            }
+
+            // récuperer l'id user de la session
+            userid = req.session.userid /// voir si suffisant (quand on aura  mongoose  pour les sessions)
+
+            console.log('list messages: session id=', req.session.id, '  session.userid=', req.session.userid) // test
+
+            messages.list_all(max_time, friends_only) //utiliser  x = await fonction... aulieu des .then 
+                .then((msgs) => res.status(201).send(msgs)) //pas la peine de retourner l'id, le status suffit
+                .catch((err) => res.status(500).send(err));
+        } catch (error) {
+            // Toute autre erreur
+            res.status(500).json({
+                //status: 500,
+                message: "lsit messages: erreur interne",
+                details: (error || "Erreur inconnue").toString()
+            })
+        }
+    })
+
+
 
 
         //.route("/messages/:user_id(\\d+)")
